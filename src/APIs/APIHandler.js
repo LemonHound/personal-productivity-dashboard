dde967daa076a9fd9d553898507b2aa0501c74b2
Kevin <kevin.zookski@gmail.com>
2024-05-29 10:38:31 -0500

    better task view

--- src/APIs/APIHandler.js
+++ src/APIs/APIHandler.js
@@ -2,55 +2,38 @@ import axios from 'axios';
 
 const API_BASE_URL = 'http://localhost:3001/api'; // Replace with the backend server's URL
 
-export const getAllUsers = async () => {
-    try {
-        const response = await axios.get(`${API_BASE_URL}/Users/FullUserDownload`);
-        return response.data;
-    } catch (error) {
-        console.error('Error fetching user data:', error);
-        throw error; // Rethrow the error to be handled by the caller
-    }
-};
+// This is the primary function of this class.  It handles the actual traffic as a standardized call.
+async function handleAPICall(path) {
 
-export const getAllTasks = async () => {
-    try {
-        const response = await axios.get(`${API_BASE_URL}/Tasks/GetAllTasks`);
-        return response.data;
-    } catch (error) {
-        console.error('Error fetching Task data:', error);
-        throw error; // Rethrow the error to be handled by the caller
-    }
-};
-
-export const getAllGoals = async () => {
-    try {
-        const response = await axios.get(`${API_BASE_URL}/Goals/GetAllGoals`);
-        return response.data;
-    } catch (error) {
-        console.error('Error fetching Goal data:', error);
-        throw error; // Rethrow the error to be handled by the caller
-    }
-};
-
-export const getAPIData = async (queryDefinition) => {
-
-    const apiUrl = "";
+    let fullURL = API_BASE_URL + path;
 
     try {
-        fetch('../Database/sql/queries/SQL_query_definitions.json')
-            .then(response => response.json())
-            .then(data => {
-                const { folder, query } = data.GoalsDownloadByUser;
-                apiUrl = API_BASE_URL + folder + query;
-            })
-            .catch(error => {
-                console.error(`Error fetching ${queryDefinition} data:`, error);
-            });
-
-        const response = await axios.get(apiUrl);
+        const response = await axios.get(fullURL);
         return response.data;
     } catch (error) {
-        console.error(`Error fetching ${queryDefinition} data:`, error);
+        console.error(`Error fetching ${path} data:`, error);
         throw error;
     }
 }
+
+
+/*
+    All API calls can now be defined with a simple URL path definition
+    Note: the path here may be different than the path to the SQL query file.
+    This path should be the path defined in the database.js file APIs section
+*/
+
+export const getAllUsers = async () => {
+    let path = "/Users/FullUserDownload";
+    return await handleAPICall(path);
+};
+
+export const getAllTasks = async () => {
+    let path = "/Tasks/GetAllTasks";
+    return await handleAPICall(path);
+};
+
+export const getAllGoals = async () => {
+    let path = "/Goals/GetAllGoals";
+    return await handleAPICall(path);
+};
